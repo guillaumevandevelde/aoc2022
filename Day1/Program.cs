c6753b5f3b89ea8c447dedc5363f847ce66ad685
Van de Velde Guillaume <Guillaume.VandeVelde@digipolis.gent>
2022-12-01 22:26:43 +0100

    Day 1 aoc

---
+++ Day1/Program.cs
@@ -0,0 +1,8 @@
+﻿var data =  File.ReadAllLines("input.txt").ToList();
+List<int> calories = data.Select(s => (s.Length > 0) ? int.Parse(s) : 0).ToList();
+
+var caloriesPerElf = new List<int>(data.Count){0};
+foreach(var calorie in calories) if (calorie == 0) caloriesPerElf.Add(0); else caloriesPerElf[caloriesPerElf.Count - 1] += calorie;
+
+Console.WriteLine($"Maximum: {caloriesPerElf.Max()}");
+Console.WriteLine($"Top 3: {caloriesPerElf.OrderByDescending(c => c).Take(3).Sum()}");
