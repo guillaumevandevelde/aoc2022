4f310d4d13c2ab25a4a282d2f5d3b4b59444594f
Van de Velde Guillaume <Guillaume.VandeVelde@digipolis.gent>
2022-12-02 16:17:16 +0100

    Day 2

---
+++ Day2/Solution 1/Program.cs
@@ -0,0 +1,42 @@
+﻿var plays = File.ReadAllLines("../Input.txt");
+int totalScore = 0;
+
+Dictionary<string, (string draw, string win, int score )> actions = 
+    new () {
+        {"X",("A","C",1)},
+        {"Y",("B","A",2)},
+        {"Z",("C","B",3)}
+    };
+
+int calculateScore(string opponent, string player)
+{
+    int result = 0;
+
+    if(opponent == actions[player].draw)
+    {
+        return result = actions[player].score + 3;
+    }
+    else if(opponent == actions[player].win)    {
+        return result = actions[player].score + 6;
+    }
+   
+    return result = actions[player].score;  
+}
+
+    
+plays.ToList().ForEach(play => {
+     totalScore += calculateScore(play.Split(" ")[0], play.Split(" ")[1]);
+});
+
+Console.WriteLine($"Total Score: {totalScore}");
+
+
+     
+
+
+
+
+
+
+
+
---
+++ Day2/Solution 2/Program.cs
@@ -0,0 +1,43 @@
+﻿var plays = File.ReadAllLines("../Input.txt");
+int totalScore = 0;
+
+Dictionary<string, (string lose, string win, int score )> actions = 
+    new () {
+        {"A",("C","B",(1))},
+        {"B",("A","C",(2))},
+        {"C",("B","A",(3))}
+    };
+
+int calculateScore(string opponent, string player)
+{
+    int result = 0;
+
+    if (player == "Z"){
+       //Win
+        return result = actions[actions[opponent].win].score + 6;
+    }   
+    else if(player == "Y"){
+        //draw
+        return result = actions[opponent].score + 3;
+    }
+        
+    return result = actions[actions[opponent].lose].score;   
+}
+
+    
+plays.ToList().ForEach(play => {
+     totalScore += calculateScore(play.Split(" ")[0], play.Split(" ")[1]);
+});
+
+Console.WriteLine($"Total Score: {totalScore}");
+
+
+     
+
+
+
+
+
+
+
+
