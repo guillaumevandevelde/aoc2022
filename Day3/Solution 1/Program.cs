c63fa653cb2692bfc3485f899a04f4ce8bbcfb97
Van de Velde Guillaume <Guillaume.VandeVelde@digipolis.gent>
2022-12-06 10:53:23 +0100

    Day 3

---
+++ Day3/Solution 1/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] rucksacks = File.ReadAllLines("../input");
+int sum = 0;
+
+rucksacks.ToList().ForEach(rucksack => {
+
+    IEnumerable<char> compartment1 = rucksack.Take(rucksack.Length / 2);
+    IEnumerable<char> compartment2 = rucksack.Skip(rucksack.Length / 2);
+    compartment1.Where(item => compartment2.Contains(item))
+                .Distinct()
+                .ToList()
+                .ForEach(item => {
+                  sum += Char.IsLower(item)
+			        ? (int)item - 96
+			        : (int)item - 38;
+                });
+    
+});
+
+Console.WriteLine(sum);
---
+++ Day3/Solution 2/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] rucksacks = File.ReadAllLines("../Input");
+int sum = 0;
+
+sum = rucksacks.Chunk(3)
+     .Select(threeSacks =>
+		{
+			var letter = threeSacks.ElementAt(0)
+						.Intersect(threeSacks.ElementAt(1))
+						.Intersect(threeSacks.ElementAt(2))
+						.Single();
+			return Char.IsLower(letter)
+			? (int)letter - 96
+			: (int)letter - 38;
+		}).Sum();
+
+Console.WriteLine(sum);
